ccad347fb41628654f6f88ef5d0b29942eef3784
Point505 <kimhaekang20@gmail.com>
2022-11-24 01:17:59 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumohs20170334/w1102volleywithviewmodel/MainActivity.kt
@@ -0,0 +1,34 @@
+package kr.ac.kumohs20170334.w1102volleywithviewmodel
+
+import android.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kr.ac.kumohs20170334.w1102volleywithviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var model:SongViewModel
+    private var songs: Array<String>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java]
+        model.requestSong()
+
+       /* model.songs.observe(this,
+            Observer<ArrayList<String>> {
+                //Toast.makeText(this, model.songs.value.toString(), Toast.LENGTH_LONG).show()
+                songs = model.songs.value?.toTypedArray()
+                binding.listSongs.adapter = ArrayAdapter<String>(this,
+                    R.layout.simple_list_item_1,
+                    songs as Array<out String>)
+            })*/
+    }
+}
